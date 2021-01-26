ba648f70d6257177e8e29c6e7b0f6c08206eb6ac
Manuel Romei <manuel.romei@raytecvision.com>
2021-01-26 17:06:06 +0100

    feat: add library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { WebSocketTunnel } from '@illgrenoble/guacamole-common-js';
+
+import { RemoteDesktopService } from 'remote-desktop';
+import {MatDialog} from '@angular/material/dialog';
+import { ClipboardModalComponent } from './components/clipboard-modal.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./themes/default.scss','./app.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'guac-demo';
+export class AppComponent implements OnInit {
+    manager: RemoteDesktopService;
+
+    constructor(private snackBar: MatSnackBar, public dialog: MatDialog) {
+
+    }
+
+    handleScreenshot(): void {
+        this.manager.createScreenshot(blob => {
+            if (blob) {
+              console.log('Not implemented');
+                //FileSaver.saveAs(blob, `screenshot.png`);
+            }
+        });
+    }
+
+    createModal(classRef) {
+        this.manager.setFocused(false);
+        let dialogRef = this.dialog.open(classRef, {
+            height: '400px',
+            width: '400px',
+            data: { manager: this.manager, text: ""}
+          });
+          return dialogRef.afterClosed();
+        // this.manager.setFocused(false);
+        // const modal = this.ngbModal.open(classRef, {
+        //     size: 'lg',
+        //     windowClass: 'modal-xxl',
+        //     container: '.ngx-remote-desktop',
+        //     keyboard: false
+        // });
+        // modal.componentInstance.manager = this.manager;
+        // return modal;
+    }
+
+    handleDisconnect(): void {
+        this.manager.getClient().disconnect();
+    }
+
+    handleEnterFullScreen() {
+        this.manager.setFullScreen(true);
+    }
+
+    handleExitFullScreen() {
+        this.manager.setFullScreen(false);
+    }
+
+    handleClipboard(): void {
+        const modal = this.createModal(ClipboardModalComponent);
+        modal.subscribe((text) => {
+            this.manager.setFocused(true);
+            if (text) {
+                this.manager.sendRemoteClipboardData(text);
+                this.snackBar.open('Sent to remote clipboard', 'OK', {
+                    duration: 2000,
+                });
+            }
+        }, () => this.manager.setFocused(true));
+    }
+
+    handleConnect() {
+        const parameters = {
+          scheme: 'telnet',
+          hostname: 'towel.blinkenlights.nl',
+          image: 'image/png',
+          audio: 'audio/L16',
+          dpi: 96,
+          width: window.screen.width,
+          height: window.screen.height
+        };
+        /*
+         * The manager will establish a connection to: 
+         * ws://localhost:8080?ws?ip={address}&image=image/png&audio=audio/L16&dpi=96&width=n&height=n
+         */
+        this.manager.connect(parameters);
+    }
+
+    ngOnInit() {
+        // Setup tunnel. The tunnel can be either: WebsocketTunnel, HTTPTunnel or ChainedTunnel
+        const tunnel = new WebSocketTunnel('ws://localhost:8080/websocket-tunnel');
+        /**
+         *  Create an instance of the remote desktop manager by 
+         *  passing in the tunnel
+         */
+        this.manager = new RemoteDesktopService(tunnel);
+        this.handleConnect();
+        this.manager.onRemoteClipboardData.subscribe(text => {
+            const snackbar = this.snackBar.open('Received from remote clipboard', 'OPEN CLIPBOARD', {
+                duration: 1500,
+            });
+            snackbar.onAction().subscribe(() => this.handleClipboard());
+        });
+        this.manager.onReconnect.subscribe(reconnect => this.handleConnect());
+
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RemoteDesktopModule } from 'remote-desktop';
+
+
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button';
+
+import {
+  ClipboardModalComponent
+} from './components/clipboard-modal.component';
+import { OverlayContainer, FullscreenOverlayContainer } from '@angular/cdk/overlay';
+import { FileSizePipe } from './pipes/file-size.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClipboardModalComponent,
+    FileSizePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RemoteDesktopModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{provide: OverlayContainer, useClass: FullscreenOverlayContainer},
+  ],
+  entryComponents: [
+    ClipboardModalComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
